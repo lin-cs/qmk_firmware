f2e5dc36ef168871f5e9114e96e27befd29bd126
lin-cs <daylan9046@gmail.com>
2021-07-24 19:19:21 -0500

    Using function pointer typedef

--- keyboards/preonic/keymaps/lin-cs/model/profile.h
+++ keyboards/preonic/keymaps/lin-cs/model/profile.h
@@ -3,7 +3,9 @@
 #include "enums.h"
 #include "tri-layer-pair.h"
 
-struct TriLayerPair profileLayers[_MORSE+1] = {
+typedef struct TriLayerPair (*trilayer_t)(enum preonic_layers, enum preonic_layers, enum preonic_layers);
+
+trilayer_t profileLayers[_MORSE+1] = {
     [_GAMING] = CreateTriLayerPair(_DEFAULT_LOWER, _DEFAULT_RAISE, _ADJUST),
     [_CODING] = CreateTriLayerPair(_DEFAULT_LOWER, _DEFAULT_RAISE, _ADJUST),
     [_STREAMING] = CreateTriLayerPair(_DEFAULT_LOWER, _DEFAULT_RAISE, _ADJUST),
